f3efbdb898390dce5926f368916de9ddf7bf5c5b
Lucas Villalba <37760788+lucasvillalbaar@users.noreply.github.com>
2023-10-10 19:34:51 -0300

    chore(logs): deleted logginghandler

--- cmd/metrics/main.go
+++ cmd/metrics/main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
-	"github.com/gorilla/handlers"
 	"github.com/lucasvillalbaar/clickup-metrics/pkg/api"
 )
 
@@ -13,7 +11,7 @@ func main() {
 
 	router := api.Init()
 	log.Println("Metrics API")
-	err := http.ListenAndServe(":8080", handlers.LoggingHandler(os.Stdout, router))
+	err := http.ListenAndServe(":8080", router)
 	if err != nil {
 		log.Fatal("Server error:", err)
 	}
